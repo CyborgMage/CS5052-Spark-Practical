bf03e60a2af41fd2ee2a677788266491082db6bc
CyborgMage <peter.goad@yahoo.com>
2021-04-05 22:07:53 +0100

    All basic search functions implemented

--- search.py
+++ search.py
@@ -1,26 +1,37 @@
 import sys
 import main
-from pyspark.sql.functions import countDistinct
+from pyspark.sql.functions import countDistinct, sum, col
 
+
+#Note: correcting user input is out of scope for this implementation.
+#More elegant error handling warranted?
 
 def search_by_userid(search_id):
-    result_movies = spark.ratings_frame.filter("userId LIKE " + search_id)
+    result_movies = spark.ratings_frame.filter(spark.ratings_frame.userId == search_id)
     movie_count = result_movies.count()
     #Note: current implementation does not split genre entries
     genre_count = result_movies.join(spark.movie_frame, "movieId").agg(countDistinct("genres")).collect()[0]["count(genres)"]
-    return "User " + str(search_id) + " watched " + str(movie_count) + " movies across " + str(genre_count) + " genres."
+    return "User {0} watched {1} movies across {2} genres.".format(str(search_id), str(movie_count), str(genre_count))
 
 
 def search_by_movieid(search_id):
-    print()
+    result_movies = spark.ratings_frame.filter(spark.ratings_frame.movieId + search_id)
+    view_count = result_movies.count()
+    average_rating = result_movies.agg(sum("rating")).collect()[0][0] / view_count
+    return "Movie {0} has an average rating of {1} across {2} viewers.".format(str(search_id), str(average_rating),
+                                                                               str(view_count))
 
 
 def search_by_title(search_id):
-    print()
+    movie_id = spark.movie_frame.filter(spark.movie_frame.title == search_id).collect()[0]["movieId"]
+    print("Movie \'{0}\' has code {1}".format(str(search_id), movie_id))
+    return search_by_movieid(movie_id)
 
 
 def search_by_genre(search_id):
-    print()
+    result_movies = spark.movie_frame.filter(col("genres").contains(search_id))
+    result_movies.show()
+    return ""
 
 
 switch_searchOption = {
