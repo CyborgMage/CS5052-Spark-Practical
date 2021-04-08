4b9c2daf85a6e1aba1135c87668562b19fa51897
CyborgMage <peter.goad@yahoo.com>
2021-04-08 17:31:25 +0100

    Changed search functions to save output as csv

--- search.py
+++ search.py
@@ -11,16 +11,17 @@ def search_by_userid(search_id):
     movie_count = result_movies.count()
     #Note: current implementation does not split genre entries
     genre_count = result_movies.join(spark.movie_frame, "movieId").agg(countDistinct("genres")).collect()[0]["count(genres)"]
-    return "User {0} watched {1} movies across {2} genres.".format(str(search_id), str(movie_count), str(genre_count))
+    print("User {0} watched {1} movies across {2} genres.".format(str(search_id), str(movie_count), str(genre_count)))
+    return result_movies
 
 
 def search_by_movieid(search_id):
     result_movies = spark.ratings_frame.filter(spark.ratings_frame.movieId + search_id)
     view_count = result_movies.count()
     average_rating = result_movies.agg(sum("rating")).collect()[0][0] / view_count
-    return "Movie {0} has an average rating of {1} across {2} viewers.".format(str(search_id), str(average_rating),
-                                                                               str(view_count))
-
+    print("Movie {0} has an average rating of {1} across {2} viewers.".format(str(search_id), str(average_rating),
+                                                                               str(view_count)))
+    return result_movies
 
 def search_by_title(search_id):
     movie_id = spark.movie_frame.filter(spark.movie_frame.title == search_id).collect()[0]["movieId"]
@@ -30,15 +31,13 @@ def search_by_title(search_id):
 
 def search_by_genre(search_id):
     result_movies = spark.movie_frame.filter(col("genres").contains(search_id))
-    result_movies.show()
-    return ""
+    return result_movies
 
 
 def search_by_useridlist(search_ids):
     movie_ids = spark.ratings_frame.filter(col("userId").isin(search_ids))
     result_movies = spark.movie_frame.join(movie_ids, spark.movie_frame.movieId == movie_ids.movieId, how="leftsemi")
-    result_movies.show()
-    return ""
+    return result_movies
 
 
 def search_by_genrelist(search_ids):
@@ -49,8 +48,7 @@ def search_by_genrelist(search_ids):
     for y in entry_results[1:]:
         list_results = list_results.union(y)
     #Ordering necessary here? Was included here to help confirm correct behaviour. Appears to order movieId as string, not number value.
-    list_results.dropDuplicates().orderBy("movieId").show()
-    return ""
+    return list_results.dropDuplicates().orderBy("movieId")
 
 
 switch_searchOption = {
@@ -73,6 +71,7 @@ if len(sys.argv) >= 3:
     spark = main.build_session()
     keepColumns = switch_searchOption[searchOption]
     search = switch_searchOption.get(searchOption, lambda: "Invalid search option")
-    print(search(searchKey))
+    search.write.csv("searchresult.csv")
+    print("Output saved to csv")
 else:
     quit(1)
