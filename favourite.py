32c2a3876881431d0b8e0104b74e71f4630cb506
nikhilahan <nap5@st-andrews.ac.uk>
2021-04-09 17:29:10 +0100

    Debugged Intermediate Point

--- favourite.py
+++ favourite.py
@@ -3,38 +3,39 @@ import main
 from pyspark.sql.functions import *
 
 def favourite_movies(user_ids):
-    movies = spark.movies_frame
+    movies = spark.movie_frame
     ratings = spark.ratings_frame.filter(col("userId").isin(user_ids))
-    user_movies = ratings.join(movies, movies.movieId == ratings.movieId).select("movieId", "genres")
+    user_movies = ratings.join(movies, on =['movieId'], how='inner').select("movieId", "genres")
     genre_dict = {}
 
     for row in user_movies.rdd.collect():
         genres = row["genres"]
         genres_list = genres.split("|")
         for genre in genres_list:
-            if genre in genre_dict.keys:
+            if genre in genre_dict:
                 x = int(genre_dict.get(genre))
                 x = x+1
                 genre_dict[genre] = str(x)
             else:
                 genre_dict[genre] = "0"
-    
+
     top_genre = []
     top_Watches = 0
 
     for genre in genre_dict:
-        if genre_dict.get(genre) > top_Watches:
+        currentValue = int(genre_dict.get(genre))
+        if  currentValue > top_Watches:
             top_genre.clear()
             top_genre.append(genre)
-            top_Watches = genre_dict.get(genre)
-        elif genre_dict.get(genre) == top_Watches:
+            top_Watches = currentValue
+        elif currentValue == top_Watches:
             top_genre.append(genre)
         else:
             continue
 
     return top_genre
 
-if len(sys.argv >= 1):
+if len(sys.argv) >= 1:
     fav_users = sys.argv[1:]
     spark = main.build_session()
     top_genres = favourite_movies(fav_users)
