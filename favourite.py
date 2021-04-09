253dd2b2569103d3d6c2984674091ecfdcde50e1
nikhilahan <nap5@st-andrews.ac.uk>
2021-04-09 18:14:30 +0100

    Minor fixes

--- favourite.py
+++ favourite.py
@@ -35,7 +35,7 @@ def favourite_movies(user_ids):
 
     return top_genre
 
-if len(sys.argv) >= 1:
+if len(sys.argv) >= 2:
     fav_users = sys.argv[1:]
     spark = main.build_session()
     top_genres = favourite_movies(fav_users)
