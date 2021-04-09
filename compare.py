b06cafc76134a06173a92587254c0847de83da01
CyborgMage <peter.goad@yahoo.com>
2021-04-09 18:24:30 +0100

    Compare bugfixes

--- compare.py
+++ compare.py
@@ -6,8 +6,8 @@ if len(sys.argv) >= 3:
     searchKey1 = sys.argv[1]
     searchKey2 = sys.argv[2]
     spark = main.build_session()
-    result_user1 = spark.ratings_frame.filter(spark.ratings_frame.userId == searchKey1).drop("userId", "timestamp")
-    result_user2 = spark.ratings_frame.filter(spark.ratings_frame.userId == searchKey2).drop("userId", "timestamp")
+    result_user1 = spark.ratings_frame.filter(spark.ratings_frame.userId == searchKey1).drop("userId", "timestamp").withColumnRenamed("rating", "rating1")
+    result_user2 = spark.ratings_frame.filter(spark.ratings_frame.userId == searchKey2).drop("userId", "timestamp").withColumnRenamed("rating", "rating2")
     # produces dataframe with both users' ratings for the same film presented alongside each other
     results = result_user1.join(result_user2, "movieId", "outer")
     results.write.format("csv").mode("overwrite").save("moviecolumns.csv")
