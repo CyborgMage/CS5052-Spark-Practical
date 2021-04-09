579b82bc9192bccfa127768329082e783c07fd83
CyborgMage <peter.goad@yahoo.com>
2021-04-09 18:52:01 +0100

    Added repartition to compare

--- compare.py
+++ compare.py
@@ -10,7 +10,7 @@ if len(sys.argv) >= 3:
     result_user2 = spark.ratings_frame.filter(spark.ratings_frame.userId == searchKey2).drop("userId", "timestamp").withColumnRenamed("rating", "rating2")
     # produces dataframe with both users' ratings for the same film presented alongside each other
     results = result_user1.join(result_user2, "movieId", "outer")
-    results.write.format("csv").mode("overwrite").save("moviecolumns.csv")
+    results.repartition(1).write.format("csv").mode("overwrite").save("moviecolumns.csv")
     # two main metrics; compare ratings for similar movies and compare amount of moves watched by one user but not the other
     results_rowmatrix = RowMatrix(results.drop("movieId").rdd.map(list))
     results_comparison = results_rowmatrix.columnSimilarities()
